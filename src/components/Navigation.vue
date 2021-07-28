63f214b2292e6e1fdb5ab73cee03f5dfff22d766
Jakz Aizzat <jakzaizzat@gmail.com>
2021-07-28 09:30:18 +0800

    Add Top Navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-green-600 py-4 flex items-center justify-center text-white">
+    <router-link to="/" class="p-2 hover:bg-white hover:text-black rounded mr-4"
+      >Products</router-link
+    >
+    <router-link to="/cart" class="p-2 hover:bg-white hover:text-black rounded"
+      >Cart ({{ cart }})</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data: () => ({
+    cart: 0,
+  }),
+};
+</script>
+
+<style scoped>
+.router-link-exact-active {
+  @apply bg-white text-black;
+}
+</style>
