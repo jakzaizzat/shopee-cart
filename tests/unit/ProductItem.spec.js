8d09a4da2c04801751466341ed368089206585e2
Jakz Aizzat <jakzaizzat@gmail.com>
2021-07-30 09:40:34 +0800

    Refactor ProductItem test

--- tests/unit/ProductItem.spec.js
+++ tests/unit/ProductItem.spec.js
@@ -7,20 +7,39 @@ describe("ProductItem.vue", () => {
     price: 29,
   };
 
-  const wrapper = shallowMount(ProductItem, {
-    propsData: {
-      product: mockProduct,
-    },
-  });
+  const factory = (product, slot = "") => {
+    return shallowMount(ProductItem, {
+      propsData: {
+        product: product,
+      },
+      slots: {
+        default: slot,
+      },
+    });
+  };
 
   it("should render product name and price", () => {
+    const wrapper = factory(mockProduct);
     expect(wrapper.find("span").text()).toContain(mockProduct.name);
     expect(wrapper.find("div").text()).toContain(mockProduct.price);
   });
 
   it("should  show quantity if there is a quantity data", () => {
+    const mockProduct = {
+      name: "Keyboard",
+      price: 29,
+      quantity: 1,
+    };
+    const wrapper = factory(mockProduct);
+
     expect(wrapper.vm).toBeTruthy();
+    expect(wrapper.find("[data-qa='product-item']").text()).toContain(
+      `(${mockProduct.quantity})`
+    );
   });
 
-  it("should render anything component in the action tab", () => {});
+  it("should render anything component in the action tab", () => {
+    const wrapper = factory(mockProduct, "<button>Button Text</button>");
+    expect(wrapper.find("button").text()).toContain("Button Text");
+  });
 });
